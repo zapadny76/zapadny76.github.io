394d1d948661d330b800359873fc03514670ad77
zapadny76 <zapadny76@rambler.ru>
2025-01-14 15:59:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Плавная прокрутка к разделам
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
